f2a8ce47511a3e63479ddd406517330484cb8bf9
DawidIzydor <admin@dawid-izydor.pl>
2018-10-13 14:17:51 +0200

    Sortowanie przez wybieranie i przez wstawianie

--- C#/AiSD/Program.cs
+++ C#/AiSD/Program.cs
@@ -6,7 +6,7 @@ namespace AiSD
     class Program
     {
 
-        public void Wyswietl(List<int> lista)
+        public void Wyswietl<T, Q>(T lista) where T: IEnumerable<Q> 
         {
             string wyswietl = "";
             foreach(var el in lista)
@@ -20,8 +20,20 @@ namespace AiSD
             Console.WriteLine(wyswietl);
         }
 
-        public Program()
+        public void Wyswietl<T>(T lista) where T:IEnumerable<int>
         {
+            Wyswietl<List<int>, int>(new List<int>(lista));
+        }
+
+        public void Wyswietl<T>(T[] lista)
+        {
+            Wyswietl<List<T>, T>(new List <T> (lista));
+        }
+
+        public void WyszukiwanieTest()
+        {
+            //TODO przygotowac dane testowe
+
             Wyszukiwanie wyszukiwanie = new Wyszukiwanie();
 
             int ZnalezionyIndex;
@@ -35,7 +47,7 @@ namespace AiSD
             ZnalezionyIndex = -1;
             Szukaj = 44;
             ZnalezionyIndex = wyszukiwanie.Liniowe(Dane.ListaDanych.ToArray(), Szukaj);
-            Console.WriteLine("Szukano " + Szukaj.ToString() + ", " + (ZnalezionyIndex >=0 ? "Znaleziono na pozycji "+ZnalezionyIndex : "Nie znaleziono"));
+            Console.WriteLine("Szukano " + Szukaj.ToString() + ", " + (ZnalezionyIndex >= 0 ? "Znaleziono na pozycji " + ZnalezionyIndex : "Nie znaleziono"));
             ZnalezionyIndex = -1;
             Szukaj = 777;
             ZnalezionyIndex = wyszukiwanie.Liniowe(Dane.ListaDanych.ToArray(), Szukaj);
@@ -80,12 +92,60 @@ namespace AiSD
             Console.WriteLine("Szukano " + Szukaj.ToString() + ", " + (ZnalezionyIndex >= 0 ? "Znaleziono na pozycji " + ZnalezionyIndex : "Nie znaleziono"));
 
 
-            Console.ReadKey();
+        }
+
+        public void SortowanieTest()
+        {
+            int[] TestowaTablica;
+
+            int[] PosortowanaTablica;
+            
+            Sortowanie sort = new Sortowanie();
+
+
+            Console.WriteLine("\nSortowanie przez proste wybieranie");
+
+            Console.WriteLine("Przed sortowaniem");
+            TestowaTablica = Dane.ListaDanych.ToArray();
+            Wyswietl(TestowaTablica);
+
+            PosortowanaTablica = sort.PrzezProsteWybieranie(TestowaTablica);
+
+            Console.WriteLine("Po posortowaniu");
+            Wyswietl(PosortowanaTablica);
+
+            
+            Console.WriteLine("\nSortowanie przez proste wstawianie");
+
+            Console.WriteLine("Przed sortowaniem");
+            TestowaTablica = Dane.ListaDanych.ToArray();
+            Wyswietl(TestowaTablica);
+
+            PosortowanaTablica = sort.PrzezProsteWstawianie(TestowaTablica);
+
+            Console.WriteLine("Po posortowaniu");
+            Wyswietl(PosortowanaTablica);
+        }
+
+        public void RunAllTests()
+        {
+            WyszukiwanieTest();
+            SortowanieTest();
+            
+        }
+
+        public Program()
+        {
+              
         }
 
         static void Main(string[] args)
         {
             Program program = new Program();
+
+            program.RunAllTests();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ C#/AiSD/Sortowanie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiSD
+{
+    class Sortowanie
+    {
+        public int[] PrzezProsteWybieranie(int[] KolecjaDoPosortowania)
+        {
+            int iloscIteracji = 0;
+
+            for(int i = 0; i <  KolecjaDoPosortowania.Length-1; ++i)
+            {
+                iloscIteracji++;
+
+                int PozycjaNajmniejszego = i;
+
+                for(int j = i + 1; j < KolecjaDoPosortowania.Length; ++j)
+                {
+                    iloscIteracji++;
+
+                    if(KolecjaDoPosortowania[j] < KolecjaDoPosortowania[PozycjaNajmniejszego])
+                    {
+                        PozycjaNajmniejszego = j;
+                    }
+                }
+
+                int temp = KolecjaDoPosortowania[PozycjaNajmniejszego];
+                KolecjaDoPosortowania[PozycjaNajmniejszego] = KolecjaDoPosortowania[i];
+                KolecjaDoPosortowania[i] = temp;
+            }
+
+            Console.WriteLine("Ilosc iteracji: " + iloscIteracji);
+
+            return KolecjaDoPosortowania;
+        }
+
+        public int[] PrzezProsteWstawianie(int[] KolekcjaDoPosortowania)
+        {
+            int iloscIteracji = 0;
+
+            for (int i = 1; i < KolekcjaDoPosortowania.Length; ++i)
+            {
+                iloscIteracji++;
+
+                int Klucz = KolekcjaDoPosortowania[i];
+
+                int j = i - 1;
+
+                while(j>=0 && KolekcjaDoPosortowania[j] > Klucz)
+                {
+                    iloscIteracji++;
+
+                    KolekcjaDoPosortowania[j + 1] = KolekcjaDoPosortowania[j];
+                    j--;
+                }
+
+                KolekcjaDoPosortowania[j + 1] = Klucz;
+            }
+
+            Console.WriteLine("Ilosc iteracji: " + iloscIteracji);
+
+            return KolekcjaDoPosortowania;
+
+        }
+
+
+    }
+}
